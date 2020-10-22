0c0c658323cd726522f7edf4f2b8cbebdfd36d92
Will Studabaker <will.studabaker@gmail.com>
2020-10-22 15:47:10 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -11,7 +11,7 @@ def input_to_index(input)
 end
 
 def valid_move?(board, move)
-  if move.between(0, 8) && !position_taken?(board, move)
+  if move.between?(0, 8) && !position_taken?(board, move)
     true
   else
     false
